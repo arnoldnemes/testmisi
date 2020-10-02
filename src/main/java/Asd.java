e73b6622ce8356d62897fa0785f787638e463b47
Nemes <arnold.nemes@accenture.com>
2020-10-02 17:29:21 +0200

    git add command

--- src/main/java/Asd.java
+++ src/main/java/Asd.java
@@ -3,5 +3,8 @@ public class Asd {
     public static void main(String[] args) {
         System.out.println("asdasd");
         System.out.println("this is a new commit");
+
+
+        System.out.println("git add");
     }
 }
